f1c239e54bfd31f5959a2d33cb18cbcd40626c17
kyo5884 <kyo5884.public@gmail.com>
2020-06-30 18:16:38 +0900

    add accent color shades

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="h-12 px-4 bg-secondary flex items-center justify-between sticky top-0 inset-x-0"
+    class="h-12 px-4 bg-accent-600 text-white flex items-center justify-between sticky top-0 inset-x-0"
   >
     <h1 class="text-lg">Remo</h1>
     <button>⚙▽</button>
