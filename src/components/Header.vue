8f22bcbd7e10f52221a42e45bfd50cb96896b988
kyo5884 <kyo5884.public@gmail.com>
2020-06-30 21:25:37 +0900

    add openInEditor button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,6 +3,16 @@
     class="h-12 px-4 bg-accent-600 text-white flex items-center justify-between sticky top-0 inset-x-0"
   >
     <h1 class="text-lg">Remo</h1>
-    <button>⚙▽</button>
+    <button @click="openSettings()">⚙</button>
   </header>
 </template>
+<script>
+import Settings from '../settings'
+export default {
+  methods: {
+    openSettings() {
+      Settings.openInEditor()
+    },
+  },
+}
+</script>
