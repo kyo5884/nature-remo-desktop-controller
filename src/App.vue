05a416a63c069a714841e167af758ad6722687b1
kyo5884 <kyo5884.public@gmail.com>
2020-06-30 16:36:46 +0900

    add code to get user's nickname

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/icon.png" />
+    <p style="color: white;">{{ username }}</p>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { Cloud } from 'nature-remo'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+  },
+  data: () => {
+    return {
+      username: null,
+    }
+  },
+  async mounted() {
+    const remo = new Cloud('ACCESS_TOKEN')
+    this.username = (await remo.getUser()).nickname
   },
 }
 </script>
