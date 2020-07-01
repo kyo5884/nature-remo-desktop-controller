fffe87bfbf410bd236f3e77d56e465a6b9f47278
kyo5884 <kyo5884.public@gmail.com>
2020-07-01 19:12:05 +0900

    show device name in Header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app" class="dark text-primary">
-    <Header />
-    <div class="m-2">
-      <img alt="Vue logo" src="./assets/icon.png" />
-      <p>{{ username }}</p>
-      <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div v-for="device in devices" :key="device.id">
+      <Header :device="device" />
+      <div class="m-2">
+        <img alt="Vue logo" src="./assets/icon.png" />
+        <p>{{ username }}</p>
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+      </div>
     </div>
   </div>
 </template>
@@ -25,11 +27,14 @@ export default {
   data: () => {
     return {
       username: null,
+      devices: [],
     }
   },
   async mounted() {
     const remo = new Cloud(Settings.get('api_token'))
     this.username = (await remo.getUser()).nickname
+    this.devices = await remo.getDevices()
+    console.log(this.devices)
   },
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,13 +2,19 @@
   <header
     class="h-12 px-4 bg-accent-600 text-white flex items-center justify-between sticky top-0 inset-x-0"
   >
-    <h1 class="text-lg">Remo</h1>
+    <h1 class="text-lg">{{ device.name }}</h1>
     <button @click="openSettings()">⚙</button>
   </header>
 </template>
 <script>
 import Settings from '../settings'
 export default {
+  props: {
+    device: {
+      type: Object,
+      required: true,
+    },
+  },
   methods: {
     openSettings() {
       Settings.openInEditor()
