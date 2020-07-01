1d0b1545d47edb9238c3b76034a935fbae95c112
kyo5884 <kyo5884.public@gmail.com>
2020-07-01 19:39:50 +0900

    add vue-material-design-icons

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
 
+import CogOutLine from 'vue-material-design-icons/CogOutline.vue'
+Vue.component('icon-cog-outline', CogOutLine)
+
 Vue.config.productionTip = false
 
 new Vue({
